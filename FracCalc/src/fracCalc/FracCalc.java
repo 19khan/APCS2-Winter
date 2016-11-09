05be9122d439100436cd34146309fbfe566321a8
19khan <17hanj19@millshighsch.org>
2016-11-09 10:29:54 -0800

    FracCalc 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -6,9 +6,10 @@ public class FracCalc {
     public static void main(String[] args){
     	
     	Scanner userInput = new Scanner (System.in);
+    	System.out.println("give equation.");
     	String inputEquation = userInput.nextLine();
     	
-    	System.out.print (produceAnswer (inputEquation));
+    	System.out.println(produceAnswer (inputEquation));
     	// TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -21,29 +22,68 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String equation)
-    { 
+    public static String produceAnswer(String equation){ 
         // TODO: Implement this function to produce the solution to the input
         String [] inputSplit = equation.split (" ");
         String equation1 = inputSplit[0];
         String operant = inputSplit[1];
-        String equation2 = inputSplit[2];
+        String equation2 = inputSplit[2];    	
         
+        String wholeNum1 = "";
+        String frac1 = "";
         String [] equation1Split = equation1.split("_");
-        int wholeNum1 = Integer.parseInt(equation1Split[0]);
-        String frac1 = equation1Split[1];
+        if (equation1Split.length <=1
+        		&& Arrays.toString(equation1Split).indexOf("/") >0){
+        	wholeNum1 = "0";
+        	frac1 = equation1Split[0];
+        }else if (equation1Split.length <=1){
+        	wholeNum1 = equation1Split[0];
+        	frac1 = equation1Split[0];
+        }else{
+        	wholeNum1 = (equation1Split[0]);
+        	frac1 = equation1Split[1];
+        }
+        
         String [] equation2Split = equation2.split("_");
-        int wholeNum2 = Integer.parseInt(equation2Split[0]);
-        String frac2 = equation2Split[1];
+        String wholeNum2 = "";
+        String frac2 = "";
+        if (equation2Split.length <=1
+        		&& Arrays.toString(equation2Split).indexOf("/") >0){
+        	wholeNum2 = "0";
+        	frac2 = equation2Split[0];
+        }else if (equation2Split.length <=1){
+        	wholeNum2 = equation2Split[0];
+        	frac2 = equation2Split[0];
+        }else{
+        	wholeNum2 = (equation2Split[0]);
+        	frac2 = equation2Split[1];
+        }
         
+        String numerator1 = "";
+        String denominator1 = "";
         String [] frac1Split = frac1.split("/");
-        int numerator1 = Integer.parseInt(frac1Split[0]);
-        int denominator1 = Integer.parseInt(frac1Split[1]);
-        String [] frac2Split = frac2.split("/");
-        int numerator2 = Integer.parseInt(frac2Split[0]);
-        int denominator2 = Integer.parseInt(frac2Split[1]);
+        if (frac1Split.length <=1){
+        	numerator1 = "0";
+        	denominator1 = "1";
+        }else{
+        	numerator1 = frac1Split[0];
+        	denominator1 = frac1Split[1];
+        }
         
-        return equation2;
+        String numerator2 = "";
+        String denominator2 = "";
+        String [] frac2Split = frac2.split("/");
+        if (frac2Split.length <=1){
+        	numerator2 = "0";
+        	denominator2 = "1";
+        }else{
+        	numerator2 = frac2Split[0];
+        	denominator2 = frac2Split[1];
+        }
+        
+        if (operant == "+"){
+        	int newWholeNum = wholeNum1 + wholeNum2;
+        return ("whole:" +wholeNum2+ " numerator:" +numerator2+ " denominator:" +denominator2);
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
